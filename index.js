f3d907fbfab82c663ba426bcfd7e1ba9e2f47f3d
jencekade <gaephuoc@gmail.com>
2017-11-07 07:52:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Code your solutions in this file
 function printBadges() {
-  
+
 }
 
 function tailsNeverFails() {
   let tails = 0;
   let coinFlip = Math.random();
   while (coinFlip >= 0.5) {
-    coinFlip = Math.randon();
+    coinFlip = Math.random();
     tails++;
   }
   return tails;
