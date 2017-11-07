6a156f591bc473ae6dff9c0f6cff32766a0e5e5d
jencekade <gaephuoc@gmail.com>
2017-11-07 07:52:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function printBadges() {
+  
+}
+
+function tailsNeverFails() {
+  let tails = 0;
+  let coinFlip = Math.random();
+  while (coinFlip >= 0.5) {
+    coinFlip = Math.randon();
+    tails++;
+  }
+  return tails;
+}
